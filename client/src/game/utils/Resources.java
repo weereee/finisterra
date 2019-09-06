eed6acf228a1e377b873b4ff010e6c7367e57c95
Guido Joaquin Tamborindeguy <guido@tamborindeguy.com.ar>
2019-09-06 00:57:48 -0300

    Clean ao java skin. Changing spells icons

--- client/src/game/utils/Resources.java
+++ client/src/game/utils/Resources.java
@@ -20,7 +20,7 @@ public class Resources {
     public static final String GAME_SHADERS_PATH = GAME_DATA_PATH + "shaders/";
     public static final String CLIENT_ICON = GAME_DATA_PATH + "icons/ao.png";
     public static final String GAME_SPELLS_PATH = "spells/";
-    
+
     // Files
     public static final String GAME_SHADERS_LIGHT = "light.png";
     public static final String CLIENT_CONFIG = "Config.json";
@@ -30,5 +30,4 @@ public class Resources {
     public static final String GAME_SOUNDS_EXTENSION = ".wav";
     public static final String GAME_MUSIC_EXTENSION = ".mp3";
     public static final String GAME_MIDI_EXTENSION = ".mid";
-    
 }
